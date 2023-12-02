e3d78dd9137ef982f25de63ba92ccbbefeb84d1c
khansa <khansagilani7@gmail.com>
2023-12-02 13:15:15 +0500

    Flatten module issues resolved

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def sideBar():
         )
 
 
+###########################################################################################
+def change_to_foundry_src():
+    current_dir = os.getcwd()
+    base_directory = f'{current_dir}/FoundryProject/src'
+    print("CWD: ", base_directory)
+    # Change the current working directory
+    os.chdir(base_directory)
+
 if __name__ == "__main__":
     foundryobj=foundry.FoundryProxy()
     auditorobj=Auditor.Auditor()
@@ -23,6 +31,8 @@ if __name__ == "__main__":
     session_state = st.session_state
     session_state.file_status = getattr(session_state,"file_status",False)
     
+
+    os.chdir("/home/khansa/Desktop/FuzzBee")
     print("current directory: ", os.getcwd())
 
     sideBar()
@@ -31,6 +41,21 @@ if __name__ == "__main__":
         foundryobj.createFoundry()
     # Input files
     auditorobj.input_files(session_state)
+
+    # When files are uploaded 
+    if session_state.file_status:
+        contractName = st.text_input("Enter contract name", value="UnstoppableLender.sol")
+        contractPath = f'FoundryProject/src/{contractName}'
+        
+        #change path to foundry source folder
+        change_to_foundry_src()
+
+    # Flatten module
+    if st.button("Flatten Smart Contract"):
+        # Flatten the smart contract
+        outputFlatten = foundryobj.flatten(contractName)
+        st.text_area("Flattened Smart contract:", outputFlatten, height=600)
+            
 
 
      
