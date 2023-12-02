2197ab7e34ca9834577993c48f32960426e4e210
khansa <khansagilani7@gmail.com>
2023-12-02 12:09:38 +0500

    Created foundry function and supporting functions

--- foundry.py
+++ foundry.py
@@ -1,13 +1,37 @@
 import subprocess
+import os
+import shutil
 
-class Foundry:
+class FoundryProxy:
+
     def flatten(contractPath):
         print("Flattening Started")
         # Command to execute
-        command = "forge flatten "+ contractPath
+        command = "forge flatten " + contractPath
         # Execute the command using subprocess
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         # Access the output
         output = result.stdout
         print("Flattening Ended")
         return output
+
+    def check_directory_existence(directory_path):
+        if os.path.isdir(directory_path):
+            return True
+        else:
+            return False
+
+    def runSubProcess(cmd, base_directory):
+        try:
+            # Run the command
+            subprocess.run(cmd, shell=True, check=True, cwd=base_directory)
+            print(f"Command '{cmd}' executed successfully in {base_directory}.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing command: {e}")
+
+    """ For Creating Foundry Project"""
+    def createFoundry():
+        current_dir = os.getcwd()
+        FoundryProxy.runSubProcess("forge init FoundryProject --no-commit", current_dir)
+
+
